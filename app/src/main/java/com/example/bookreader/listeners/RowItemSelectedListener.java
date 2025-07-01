3e9abdf31d1612b8c0fc74b41e37cba7ff689b9d
OlexKov <Kovalchuk.olex@gmail.com>
2025-07-02 01:12:52 +0300

    Fix update

--- app/src/main/java/com/example/bookreader/listeners/RowItemSelectedListener.java
+++ app/src/main/java/com/example/bookreader/listeners/RowItemSelectedListener.java
@@ -16,14 +16,14 @@ public class RowItemSelectedListener implements OnItemViewSelectedListener {
     public void onItemSelected(Presenter.ViewHolder itemViewHolder, Object item, RowPresenter.ViewHolder rowViewHolder, Row row) {
         if (row instanceof ListRow) {
             ListRow rowList = (ListRow)row;
-            if(rowList != app.getSelectedRow()){
+            if(app.getSelectedRow() != null && rowList != app.getSelectedRow()){
                 app.setSelectedRow(rowList);
                 app.getGlobalEventListener().sendEvent(GlobalEventType.ROW_SELECTED_CHANGE,row);
             }
         }
         if (item instanceof BookDto) {
             BookDto book = (BookDto)item;
-            if(book != app.getSelectedItem()){
+            if(app.getSelectedItem() != null && book != app.getSelectedItem()){
                 app.setSelectedItem(book);
                 app.getGlobalEventListener().sendEvent(GlobalEventType.ITEM_SELECTED_CHANGE,book);
             }
