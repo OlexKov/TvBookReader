35941de3db05f88f502c6ab51cd4adad93dcc2ae
OlexKov <Kovalchuk.olex@gmail.com>
2025-07-20 17:27:01 +0300

    BookDtoDiffCallback equals fix

--- app/src/main/java/com/example/bookreader/diffcallbacks/BookDtoDiffCallback.java
+++ app/src/main/java/com/example/bookreader/diffcallbacks/BookDtoDiffCallback.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import androidx.leanback.widget.DiffCallback;
 import com.example.bookreader.data.database.dto.BookDto;
 
+import java.util.Objects;
+
 public class BookDtoDiffCallback extends DiffCallback<BookDto> {
     @Override
     public boolean areItemsTheSame(BookDto oldItem, BookDto newItem) {
@@ -13,14 +15,14 @@ public class BookDtoDiffCallback extends DiffCallback<BookDto> {
     @Override
     public boolean areContentsTheSame(BookDto oldItem, BookDto newItem) {
         return oldItem.title.equals(newItem.title)
+                && Objects.equals(oldItem.year,newItem.year)
+                && Objects.equals(oldItem.categoryId,newItem.categoryId)
+                && Objects.equals(oldItem.author,newItem.author)
+                && Objects.equals(oldItem.description,newItem.description)
+                && Objects.equals(oldItem.creationDate,newItem.creationDate)
+                && Objects.equals(oldItem.filePath,newItem.filePath)
+                && Objects.equals(oldItem.previewPath,newItem.previewPath)
                 && (oldItem.isFavorite == newItem.isFavorite)
-                && (oldItem.year == newItem.year)
-                && (oldItem.pageCount == newItem.pageCount)
-                && (oldItem.categoryId == newItem.categoryId)
-                && oldItem.author.equals(newItem.author)
-                && oldItem.description.equals(newItem.description)
-                && oldItem.creationDate.equals(newItem.creationDate)
-                && oldItem.filePath.equals(newItem.filePath)
-                && oldItem.previewPath.equals(newItem.previewPath);
+                && (oldItem.pageCount == newItem.pageCount);
     }
 }
